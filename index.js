d3a8aeb3a31edb2383f886a415c4db033d624039
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-04-13 03:44:32 -0400

    implemented /put endpoint that replaces an existing secret given an id; added error handling for /post method so that a user is promoted to add values for secret and score if they live one or both of them blank.

--- index.js
+++ index.js
@@ -39,39 +39,90 @@ app.post("/get-secret", async (req, res) => {
 });
 
 app.post("/post-secret", async (req, res) => {
-  // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
-  console.log("req.body = ", req.body);
-  try {
-    const createdSecret = await axios.post(
-      `${API_URL}/secrets`,
-      {
-        // create a new secret with the input from the request
-        secret: req.body.secret,
-        score: req.body.score,
-      },
-      // Must have a bearer token in the header of the request in 
-      // order to send a post request to create a secret with this API
-      config
-    );
-    const result = createdSecret.data;
-    console.log('createdSecret.data = ', result);
-    // Use JSON.stringify to turn the JS object from axios into a string.
-    let resString = JSON.stringify(result);
-    // send the string of the created secret to the EJS file
-    res.render('index', {
-      content: resString
+  // Throw error if the secret and score are both empty; these are MANDATORY fields to create a secret
+  if (
+    req.body.secret.trim().length === 0 ||
+    req.body.score.trim().length === 0
+  ) {
+    res.render("index", {
+      content: `Please add a secret AND a score`,
     });
-  } catch (error) {
-    console.log("Error: ", error);
-    res.render('index', {
-      content: error.message,
-    });
+  } else {
+    // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+    console.log("req.body = ", req.body);
+    try {
+      const createdSecret = await axios.post(
+        `${API_URL}/secrets`,
+        {
+          // create a new secret with the input from the request
+          secret: req.body.secret,
+          score: req.body.score,
+        },
+        // Must have a bearer token in the header of the request in
+        // order to send a post request to create a secret with this API
+        config
+      );
+      const result = createdSecret.data;
+      console.log("createdSecret.data = ", result);
+      // Use JSON.stringify to turn the JS object from axios into a string.
+      let resString = JSON.stringify(result);
+      // send the string of the created secret to the EJS file
+      res.render("index", {
+        content: resString,
+      });
+    } catch (error) {
+      console.log("Error: ", error);
+      res.render("index", {
+        content: error.message,
+      });
+    }
   }
 });
 
 app.post("/put-secret", async (req, res) => {
   const searchId = req.body.id;
-  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+  // Throw error if the secret and score are both empty; should not be able to replace an existing secret with empty values
+  if (
+    req.body.secret.trim().length === 0 ||
+    req.body.score.trim().length === 0
+  ) {
+    res.render("index", {
+      content: `Please add a secret AND a score so the secret with id = ${searchId} can be replaced.`,
+    });
+  } else {
+    // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
+    try {
+      // retrieve the secret with id = searchId
+      const result = await axios.put(
+        API_URL + "/secrets/" + searchId,
+        {
+          // update its contents with the following
+          secret: req.body.secret,
+          score: req.body.score,
+        },
+        config
+      );
+
+      const updatedSecret = result.data;
+      console.log("updatedSecret = ", updatedSecret);
+      // Use JSON.stringify to turn the JS object from axios into a string.
+      let resString = JSON.stringify(updatedSecret);
+
+      // send the string of the updated secret to the EJS file
+      res.render("index", {
+        content: resString,
+      });
+    } catch (error) {
+      console.log("Error: ", error.response.data);
+
+      // Use JSON.stringify to turn the JS object from axios into a string.
+      let errString = JSON.stringify(error.response.data);
+
+      res.render("index", {
+        content: errString,
+      });
+    }
+  }
 });
 
 app.post("/patch-secret", async (req, res) => {
